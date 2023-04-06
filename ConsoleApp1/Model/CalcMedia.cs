2e48bc84e7cf81d88148ee86ddc37bf60d3ad821
pczin <pczin@DESKTOP-KRRHA8I>
2023-04-06 17:07:40 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Model/CalcMedia.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Pratica.Model
+{
+    internal class CalcMedia
+    {
+        public static decimal _Media = 7m;
+        public decimal _maiorNota;
+        public decimal _SomaNota;
+        public decimal _MediaAluno;
+        public decimal _nota01;
+        public decimal _nota02;
+        public string _resultado;
+
+        public CalcMedia(decimal nota1, decimal nota2)
+        {
+            _nota01 = nota1;
+            _nota02 = nota2;
+
+            _SomaNota = nota1 + nota2;
+            _MediaAluno = _SomaNota / 2;
+
+            if (_MediaAluno >= _Media)
+            {
+                _resultado = "Aprovado";
+            }
+            else
+            {
+                _resultado = "Reprovado";
+            }
+
+            if (nota1 > nota2)
+            {
+                _maiorNota = nota1;
+            }
+            else
+            {
+                _maiorNota = nota2;
+            }
+        }
+
+        public decimal Media
+        {
+            get => _MediaAluno;
+            private set => _MediaAluno = value;
+        }
+
+        public decimal MaiorNota
+        {
+            get => _maiorNota;
+            private set => _maiorNota = value;
+        }
+        public string Resultado
+        {
+            get => _resultado;
+            private set => _resultado = value;
+        }
+        public Alunos Aluno { get; set; }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Atividade_Pratica.Model;
+
+Alunos aluno = new Alunos("Neilson Santos", 202110508);
+Alunos alunoNotas = new Alunos(7.5m, 8.0m, aluno);
+CalcMedia Media = new CalcMedia(alunoNotas.Nota1, alunoNotas.Nota2);
+
+Console.WriteLine($"Aluno: {aluno.Nome}, Matricula: {aluno.Matricula} \n" +
+    $"Primeira nota: {Media._nota01} \n" +
+    $"Segunda nota: {Media._nota02} \n" +
+    $"Média do aluno {aluno.Nome}: {Media.Media} \n" +
+    $"Maior nota: {Media.MaiorNota} \n" +
+    $"O aluno está {Media.Resultado}");
